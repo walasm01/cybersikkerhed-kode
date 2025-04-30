529b0ca8a2b294de8f9a98ece7e9daaca434ab2e
Walter Liao Asmussen <walasm01@iba.dk>
2025-04-30 12:01:53 +0200

    Har startet på JS. Knapperne har nu en funktion.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 'use strict';
 
-const btns = document.querySelectorAll('.btn-group');
+// Jeg henter elementer fra DOM
+const btns = document.querySelectorAll('.btn');
+const text = document.querySelector('#text');
+const what = document.querySelector('#what');
+const messageImg = document.querySelector('#text-group img');
+
+
+// Denne funktion udføres, når de enkelte knapper klikkes
+function nextScenario(e) {
+    if (e.target.id === "btn1") {
+        console.log("btn1");
+    } else if (e.target.id === "btn2") {
+        console.log("btn2");
+    } else {
+        console.log("btn3");
+    };
+};
+
+
+// Giver kommando for når knapperne klikkes
+btns.forEach(btn => {
+    btn.addEventListener('click', nextScenario);
+});
